9a6ac999d62fc478fb302f5c944cc7b33e81a6f1
Jevan Potgieter <Jevan.Potgieter@vista.co>
2019-06-25 10:10:45 +1200

    removing firebase

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js </a>. Deployed using: <a href="https://firebase.google.com/">Google Firebase</a>. </p>
+        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js </a>. Deployed using: <a href="https://www.netlify.com/">netlify</a>. </p>
     </footer>
 )
 
