8cd1bced3236025ea46bb26a3cf9ca21ab8818b2
Jevan Potgieter <Jevan.Potgieter@vista.co>
2019-06-28 14:40:49 +1200

    up
    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Diamond Nutrition",
+    author: "Jevan Potgieter",
+    description: "We help you get back on the wagon"
   },
   plugins: [
     
